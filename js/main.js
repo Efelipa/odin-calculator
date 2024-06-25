9c65b54ed29cccb0b198726ba8805657760ba3df
Erick Felipa <erickfelipa@gmail.com>
2024-06-24 22:32:11 -0500

    Reorganize the code

--- js/main.js
+++ js/main.js
@@ -1,26 +1,92 @@
-const display = document.querySelector('#display');
-console.log(display.textContent);
-
-const operate = (num1, num2, operator) => {
-    if(operator === 'add') {
-        alert(add(num1, num2));
+document.addEventListener('DOMContentLoaded', () => {
+    // Set the base values
+    const display = document.querySelector('#display');
+    const buttonsCalculator = document.querySelectorAll('.numbers button[value]')
+    const operatorsCalculator = document.querySelectorAll('.operators button');
+    // Turn the NodeList into an array
+    const buttons = [...buttonsCalculator];
+    const operators = [... operatorsCalculator];
+    // Operations 
+    const operations = {
+        add: (num1, num2) => num1 + num2,
+        subtract: (num1, num2) => num1 - num2,
+        multiply: (num1, num2) => num1 * num2,
+        divide: (num1, num2) => num1 / num2,
     }
-}
 
-const add = (num1, num2) => {
-    return num1 + num2;
-}
+    const operate = (operator,num1, num2) => {
+        switch (operator) {
+            case '+':
+                return add(num1, num2);
+            case '-':
+                return subtract(num1, num2);
+            case '*':
+                return multiply(num1, num2);
+            case '/':
+                return divide(num1, num2);
+            default:
+                break;
+        }
+    }
 
-const subtract = (num1, num2) => {
-    return num1 - num2;
-}
+    const addNumber = (num) => {
+        if(!operator) {
+            num1 +=num.substring(0,9);
+            display.textContent = num1;
+        } else if (sum !== '') {
+            num1 += num.substring(0,9);
+            display.textContent = num1;
+        } else if (num1 === '' && sum !== '' && num2 === '') {
+            num2 += num.substring(0,9);
+            display.textContent = num2;
+        } else {
+            num2 += num.substring(0,9);
+            display.textContent = num2;
+        }
+        console.log(num1);
+        console.log(num2);
+        
+    }
 
-const multiply = (num1, num2) => {
-    return num1 * num2;
-}
+    const getOperators = (symbol) => {
+        if(sum === '') {
+            operator += symbol;
+            display.textContent = num2;
+        } else if (sum !== '') {
+            operator += symbol;
+            display.textContent = num1;
+        }
+        if(operator.length > 1) {
+            if(sum === '') {
+                let newNum1 = parseInt(num1);
+                let newNum2 = parseInt(num2);
+                sum = operate(operator, newNum1, newNum2);
+                num1 = '';
+                num2 = '';
+                display.textContent = sum;
+            } else if (sum !== '') {
+                let newNum1 = parseInt(num1);
+                sum = operate(operator, sum, newNum1);
+                display.textContent = sum;
+            }
+        }
+    }
+    // Values
+    let num1 = '';
+    let num2 = '';
+    let operator = '';
+    let sum = '';
 
-const divide = (num1, num2) => {
-    return num1 / num2;
-}
-
-document.addEventListener('DOMContentLoaded', operate(num1, num2, operator));
+    buttons.forEach(button => {
+        button.addEventListener('click', (e) =>{
+            let target = e.target;
+            return addNumber(target.textContent);
+        });
+    })
+    operators.forEach(operator => {
+        operator.addEventListener('click', (e) =>{
+            let target = e.target;
+            console.log(target.textContent)
+        })
+    })
+})
